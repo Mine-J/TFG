348a8226658b093cd82da15d8a16736cd2be9368
Mine-J <jsaezg@alumnos.nebrija.es>
2025-12-17 22:26:34 +0100

    chore: update dependencies and Fresh manifest

--- apps/web/fresh.gen.ts
+++ apps/web/fresh.gen.ts
@@ -4,19 +4,43 @@
 
 import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
+import * as $_layout from "./routes/_layout.tsx";
+import * as $_middleware from "./routes/_middleware.ts";
+import * as $api_login from "./routes/api/login.ts";
+import * as $api_logout from "./routes/api/logout.ts";
+import * as $api_obtenerDatos from "./routes/api/obtenerDatos.ts";
+import * as $api_register from "./routes/api/register.ts";
+import * as $auth_middleware from "./routes/auth/_middleware.ts";
+import * as $auth_login from "./routes/auth/login.tsx";
+import * as $auth_register from "./routes/auth/register.tsx";
 import * as $greet_name_ from "./routes/greet/[name].tsx";
 import * as $index from "./routes/index.tsx";
-
+import * as $Header from "./islands/Header.tsx";
+import * as $LoginForm from "./islands/LoginForm.tsx";
+import * as $RegisterForm from "./islands/RegisterForm.tsx";
 import type { Manifest } from "$fresh/server.ts";
 
 const manifest = {
   routes: {
     "./routes/_404.tsx": $_404,
     "./routes/_app.tsx": $_app,
+    "./routes/_layout.tsx": $_layout,
+    "./routes/_middleware.ts": $_middleware,
+    "./routes/api/login.ts": $api_login,
+    "./routes/api/logout.ts": $api_logout,
+    "./routes/api/obtenerDatos.ts": $api_obtenerDatos,
+    "./routes/api/register.ts": $api_register,
+    "./routes/auth/_middleware.ts": $auth_middleware,
+    "./routes/auth/login.tsx": $auth_login,
+    "./routes/auth/register.tsx": $auth_register,
     "./routes/greet/[name].tsx": $greet_name_,
     "./routes/index.tsx": $index,
   },
-  islands: {},
+  islands: {
+    "./islands/Header.tsx": $Header,
+    "./islands/LoginForm.tsx": $LoginForm,
+    "./islands/RegisterForm.tsx": $RegisterForm,
+  },
   baseUrl: import.meta.url,
 } satisfies Manifest;
 
