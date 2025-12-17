ab847bd2e169ad6a2482d69c8c0e09d64095235f
Mine-J <jsaezg@alumnos.nebrija.es>
2025-12-17 22:45:46 +0100

    style: update login and register page styling

--- apps/web/routes/auth/login.tsx
+++ apps/web/routes/auth/login.tsx
@@ -2,8 +2,6 @@ import { LoginForm } from "../../islands/LoginForm.tsx";
 
 export default function LoginPage() {
   return (
-    <div class="contenedorPrincipal">
       <LoginForm />
-    </div>
   );
 }
--- apps/web/routes/auth/register.tsx
+++ apps/web/routes/auth/register.tsx
@@ -2,8 +2,6 @@ import {RegisterForm} from "../../islands/RegisterForm.tsx";
 
 export default function RegisterPage() {
   return (
-    <div class="contenedorPrincipal">
       <RegisterForm />
-    </div>
   );
 }
