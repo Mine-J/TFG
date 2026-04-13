171608cd38e921b2594bb5804c9d8eefddeaea6c
Mine-J <jsaezg@alumnos.nebrija.es>
2026-04-13 13:03:17 +0200

    chore: actualizar año del footer a 2026

--- apps/web/components/Footer.tsx
+++ apps/web/components/Footer.tsx
@@ -3,7 +3,7 @@ import { FunctionalComponent } from "preact/src/index.d.ts";
 export const Footer: FunctionalComponent = () => {
   return (
     <div class = "footer">
-      <p>&copy; Javier Sáez García | nebrija 2025</p>
+      <p>&copy; Javier Sáez García | nebrija 2026</p>
     </div>
   );
 };
