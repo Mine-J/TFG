c39989726a6bcbe0bd1199db16727e360ecde166
Mine-J <jsaezg@alumnos.nebrija.es>
2026-04-23 11:27:43 +0200

    arreglar texto del pie de pagina

--- apps/web/components/Footer.tsx
+++ apps/web/components/Footer.tsx
@@ -2,8 +2,8 @@ import { FunctionalComponent } from "preact/src/index.d.ts";
 
 export const Footer: FunctionalComponent = () => {
   return (
-    <div class = "footer">
-      <p>&copy; Javier Sáez García | nebrija 2026</p>
+    <div class="footer">
+      <p>&copy; Javier Sáez García | Proyecto de Fin de Grado - Universidad Nebrija, 2026</p>
     </div>
   );
 };
