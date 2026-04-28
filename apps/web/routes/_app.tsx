29945afddc3ed7156e210567411ae27e041b9f01
Mine-J <jsaezg@alumnos.nebrija.es>
2026-04-28 22:50:03 +0200

    feat(app): agregar logo y mapbox
    - Añadir logo1.png como favicon de la aplicación
    - Incluir estilos y scripts de Mapbox GL JS v3.3.0
    - Cambiar título de la aplicación a 'FarmaFinder'

--- apps/web/routes/_app.tsx
+++ apps/web/routes/_app.tsx
@@ -3,9 +3,12 @@ export default function App({ Component }: PageProps) {
   return (
     <html>
       <head>
+        <link href="https://api.mapbox.com/mapbox-gl-js/v3.3.0/mapbox-gl.css" rel="stylesheet" />
+        <script src="https://api.mapbox.com/mapbox-gl-js/v3.3.0/mapbox-gl.js"></script>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>TFG</title>
+        <title>FarmaFinder</title>
+        <link rel="icon" type="image/png" href="/logo1.png" />
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
